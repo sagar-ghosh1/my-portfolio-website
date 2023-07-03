3e38c038dae974df6349e0fa675fa0c48dd6fe75
sagar-ghosh1 <sagorghosh520@gmail.com>
2023-07-03 13:18:25 +0600

    hosting project

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,15 +5,13 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyAULVvK3YcWFYUvMDJ2dFg0RFrdq0aqimE",
-    authDomain: "portfolio-website-34ea5.firebaseapp.com",
-    projectId: "portfolio-website-34ea5",
-    storageBucket: "portfolio-website-34ea5.appspot.com",
-    messagingSenderId: "778399723306",
-    appId: "1:778399723306:web:e83fdcbd431de286f3b853"
+    apiKey: import.meta.env.VITE_APIKEY,
+    authDomain: import.meta.env.VITE_AUTHDOMAIN,
+    projectId: import.meta.env.VITE_PROJECTID,
+    storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+    appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-export default app;
