5258793c6954f69c4a81b7e1b6a5a61ec39e1ea3
sagar-ghosh1 <sagorghosh520@gmail.com>
2023-07-03 11:54:33 +0600

    banner section

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,6 @@ import Design_Info from './Design_Info/Design_Info'
 import Footer from './Footer/Footer'
 import Header from './Header/Header'
 import Portfolio_section from './Portfolio_section/Portfolio_section'
-import Resume_section from './Resume_section/Resume_section'
 import ScrollToTop from './ScrollToTop/ScrollToTop'
 import Scroll_Indicator from './Scroll_Indicator/Scroll_Indicator'
 import Service_section from './Service_section/Service_section'
@@ -39,7 +38,6 @@ function App() {
           <About_section />
           {/* <Portfolio_section /> */}
           <Service_section />
-          <Resume_section />
           <Contact_section />
           <Footer />
         </>
