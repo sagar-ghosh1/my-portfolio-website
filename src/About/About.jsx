c62f25ba08b2e71cdd0c52a2645110cd9b9c0648
sagar-ghosh1 <sagorghosh520@gmail.com>
2023-07-06 02:28:00 +0600

    secon

--- src/About/About.jsx
+++ src/About/About.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './About.css'
 import myImage from '../assets/aboutsect.png'
+import resume from "../../src/assets/Sagar_Ghosh_Resume.pdf"
 
 const About = () => {
     return (
@@ -17,6 +18,21 @@ const About = () => {
                         By writing clean code and problem- solving effectively,
                         I aim to contribute to organizational success
                         while advancing my career in web development.</p>
+
+                    <div className="info">
+                        <a href="#contact" className="contact-me"><span className="btn">Hire Me</span></a>
+                        <ul>
+                            <li>
+                                <a href="https://web.facebook.com/profile.php?id=100012189210083" target="_blank" rel="noopener noreferrer"><i className="fab fa-facebook-f"></i></a>
+                            </li>
+                            <li>
+                                <a href="https://github.com/sagar-ghosh1" target="_blank" rel="noopener noreferrer"><i className="fa-brands fa-github"></i></a>
+                            </li>
+                            <li>
+                                <a href="https://www.linkedin.com/in/sagar-ghosh-b92585280/" target="_blank" rel="noopener noreferrer"><i className="fab fa-linkedin-in"></i></a>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>
